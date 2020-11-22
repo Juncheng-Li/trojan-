30eaeba6875ef24a401f23ce0115af4282e330ca
Juncheng-Li <juncheng.alex.li@gmail.com>
2020-11-22 18:16:59 +0800

    shells 1 2 added

---
+++ step1.sh
@@ -0,0 +1,4 @@
+sudo yum -y install wget
+wget --no-check-certificate https://raw.githubusercontent.com/tcp-nanqinlang/general/master/General/CentOS/bash/tcp_nanqinlang-1.3.2.sh
+sudo bash tcp_nanqinlang-1.3.2.sh
+sudo reboot
---
+++ step2.sh
@@ -0,0 +1,5 @@
+sudo bash tcp_nanqinlang-1.3.2.sh
+sudo passwd
+su root
+curl -O https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh && chmod +x trojan_mult.sh
+./trojan_mult.sh
